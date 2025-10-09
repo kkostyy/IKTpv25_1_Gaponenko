90874d349735a217ba46951e23c5cc375713b4da
opilane <opilane@HP-CZC2349HTB>
2025-10-09 09:42:25 +0300

    New Update File

--- IKTpv25_1_1_Gaponenko_Vgade_Otsing.py
+++ IKTpv25_1_1_Gaponenko_Vgade_Otsing.py
@@ -1,31 +1,34 @@
-from math import * #vale import math
+from math import *
+
 print("Ruudu karakteristikud")
-a=float('Sisesta ruudu külje pikkus => ') 
-S=a**2
+a = float(input("Sisesta ruudu külje pikkus => "))  # Added input()
+S = a**2
 print("Ruudu pindala", round(S, 1))
-P=4*a
+P = 4 * a
 print("Ruudu ümbermõõt", round(P, 1))  
-di=a*math.sqrt(2)
-print("Ruudu diagonaal", round(di,2))
+di = a * sqrt(2)  # Removed math. since we used from math import *
+print("Ruudu diagonaal", round(di, 2))
 print()
+
 print("Ristküliku karakteristikud") 
-b=float(input("Sisesta ristküliku 1. külje pikkus => "))
-c=float(input("Sisesta ristküliku 2. külje pikkus => "))
-S=b*c
+b = float(input("Sisesta ristküliku 1. külje pikkus => "))
+c = float(input("Sisesta ristküliku 2. külje pikkus => "))
+S = b * c
 print("Ristküliku pindala", round(S, 1)) 
-P=2*(b+c)
+P = 2 * (b + c)
 print("Ristküliku ümbermõõt", P)
-di=math.sqrt(b**2+c**2)
+di = sqrt(b**2 + c**2)  # Removed math.
 print("Ristküliku diagonaal", round(di, 2)) 
 print()
-print("Ringi karakteristikud", round(d, 1))
-r=float(input("Sisesta ringi raadiusi pikkus => ")) 
-d=2*r 
+
+print("Ringi karakteristikud")  # Removed round(d, 1) - d wasn't defined yet
+r = float(input("Sisesta ringi raadiusi pikkus => ")) 
+d = 2 * r 
 print("Ringi läbimõõt", d) 
-S= pi*r**2
+S = pi * r**2
 print("Ringi pindala", round(S, 1))
-C=2*pi*r
-print("Ringjoone pikkus", round(C, 2)) 
+C = 2 * pi * r
+print("Ringjoone pikkus", round(C, 2))
 
 
 
