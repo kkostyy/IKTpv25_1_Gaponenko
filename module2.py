fc8ded52a1c719e2f529b47cc70122e048fefc0a
opilane <opilane@HP-CZC2349HTB>
2025-11-20 09:57:23 +0200

    3part

--- module2.py
+++ module2.py
@@ -1,7 +1,10 @@
 l=[]
 print(f"Listi algseis: {l}")
 while True:
-    print("1. Lisa element")
+    print("Tee valik:")
+    print("1-Lisa elemente\2-Lisa elemente pos-le\3-Emalda elementepos jargi")
+    print("")
+
     while True:
         try:
             valik=int(input())
@@ -35,4 +38,27 @@ while True:
                 print("Täisarvud on vaja kasutada")
         element = input("Sisesta element mida soovid lisada: ")
         l.insert(pos, element)
-        print(f"Uuendatud list on {l}")
+    elif valik==3:
+        while True:
+            try:
+                pos=int(input(f"Positsioon kuhu soovid emalda (0-{len(l)-1}): "))
+                if 0<=pos<=len(l)-1:
+                    break
+                else:
+                    print(f"Positsioon peab olema vahemikus 0 kuni {len(l)-1}")
+            except:
+                print("Täisarvud on vaja kasutada")
+        eem.element = l.pop(pos)
+        print(f"Eemaldatud element on {eem_element}")
+    elif valik==4:
+        element = input("Sisesta element mida soovid eemaldada: ")
+        mitu = l.count(element)
+        if mitu == 0:
+            print("Elementi ei leitud")
+        else:
+            for e in range(mitu):
+                print(f"Eemaldatud element '{element}' {l.index(element)} poeitsioonilt")
+                l.remove(element)
+        print(f"Eemaldatud {mitu} elementi")
+
+    print(f"Uuendatud list on {l}")
